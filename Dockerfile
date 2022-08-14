5276e2edbc01f0645a142898f27c4796b804cbe8
papitokarlo <begadze.zura@gmail.com>
2022-08-14 11:40:56 +0400

    added docker  https://hub.docker.com/repository/docker/papitokarlo/authentication

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /UFP
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt'
 
-
 COPY . .
 
 CMD [ "python", "-m" , "flask", "run", "--host=0.0.0.0"]
--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from auth import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
+
+#pip freeze > requirements.txt or pipreqs>requirements.txt
