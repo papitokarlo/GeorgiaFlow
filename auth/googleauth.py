626ae88df688e6c78e19c021e5ed8e7f777a13fc
papitokarlo <begadze.zura@gmail.com>
2022-10-15 00:23:43 +0400

    google authentification

--- auth/googleauth.py
+++ auth/googleauth.py
@@ -2,11 +2,15 @@ import os
 import pathlib
 
 from flask import Blueprint, render_template, flash, redirect, url_for, request, session, abort
+from flask_login import login_user, current_user
 from google_auth_oauthlib.flow import Flow
 from google.oauth2 import id_token
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
 import requests
+
+from .models import User
+from . import db
 
 google_auth = Blueprint("google_auth", __name__)
 
@@ -38,10 +42,40 @@ def google_callback():
         audience=GOOGLE_CLIENT_ID
     )
 
-    # return id_info
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
+
+    email = id_info.get("email")
+    fullname = id_info.get("name")
+    password = id_info.get("password")
+
+    if User.query.filter_by(email=email).first():
+
+        user = User.query.filter_by(email=email).first()
+        login_user(user)
+
+        flash(f'{current_user.fullname} loged successfuly', category='success')
+
+        next = request.args.get('next')
+        if next == None or not next[0] == '/':
+            next = url_for('api.index')
+        return redirect(next)
+
+    else:
+
+        from werkzeug.security import generate_password_hash
+
+        linkedin, github = 'None', 'None'
+        password_hash = generate_password_hash('password')
+
+        user = User(fullname, email, linkedin, github, password_hash)
+
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)   
+
     return redirect(url_for("api.index"))
+    
 
 @google_auth.route("/google-login")
 def google_login():
